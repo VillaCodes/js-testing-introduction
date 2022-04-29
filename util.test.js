23bcf415ff1729075808da9db23838e1c1bd3b75
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-04-29 01:51:12 -0700

    Full testing
    Unit, Integration, and e2e testing via puppeteer initialized.

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const {generateText, checkAndGenerate} = require('./util');
 
 test('should output name and age', () => {
@@ -16,3 +17,24 @@ test('should generate a valid text output', () => {
     const text = checkAndGenerate('Max', 29)
     expect(text).toBe('Max (29 years old)')
 });
+
+//browser returns a promise, so use async/await with puppeteer
+//page is an object
+//all browser related things are async
+//test also takes a timeout limit argument
+test('should create an element with text and correct class', async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo:30,
+        args: ['--window-size=1920,1080']
+    });
+    const page = await (await browser).newPage();
+    await page.goto('file:///C:/Users/johna/Documents/GitHub/js-testing-introduction/index.html')
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+    await page.click('input#age');
+    await page.type('input#age', '30');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', element => element.textContent);
+    expect(finalText).toBe('Anna (30 years old)');
+}, 10000);
